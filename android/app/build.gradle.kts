46d9c80074c50bb22de7b375493ebb1f5f0c394f
ROHIT <rohitg2800@yahoo.com>
2026-06-02 09:01:34 +0530

    chore: rename package com.equinox_bh.android → in.rohitg28.floodwatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ val keystorePropertiesFile = rootProject.file("keystore.properties")
 val useReleaseKeystore = keystorePropertiesFile.exists()
 
 android {
-    namespace = "com.equinox_bh.android"
+    namespace = "in.rohitg28.floodwatch"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -58,7 +58,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.equinox_bh.android"
+        applicationId = "in.rohitg28.floodwatch"
         minSdk    = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 2
